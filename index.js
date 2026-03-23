8b340bfdc9c3f6ba7257c7b57d8fda0fe8c3d2bb
Joelmir Siqueira <joelmirgth10@gmail.com>
2026-03-23 16:34:17 -0300

    feat: adição de exemplos  no banco de dados em memoria

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const PORT = 3000;
 app.use(express.json());
 
 // Simula um "banco de dados" em memória
-const filmes = [];
+const filmes = [
+    { titulo: "Matrix", ano: 1999 },
+    { titulo: "Interestelar", ano: 2014 },
+    { titulo: "O Senhor dos Anéis: A Sociedade do Anel", ano: 2001 },
+    { titulo: "A Origem", ano: 2010 }
+];
 
 app.get('/filmes', (req, res) => {
     res.status(200).json(filmes);
