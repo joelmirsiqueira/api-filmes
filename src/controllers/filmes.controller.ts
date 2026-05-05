e86b5f982baf8ac836ef4f470c3a4a17f6c334cd
JalmirSiqueira <jalmirgth10@gmail.com>
2026-05-05 15:48:21 -0300

    fix: correção de tratamento de erro no controller da rota de delete

--- src/controllers/filmes.controller.ts
+++ src/controllers/filmes.controller.ts
@@ -2,24 +2,23 @@ import { Request, Response } from "express";
 import * as filmesService from "../services/filmes.service.js";
 
 export const obterFilmes = (req: Request, res: Response): Response => {
-    const filmes = filmesService.obterTodosFilmes();
-    return res.status(200).json(filmes);
+  const filmes = filmesService.obterTodosFilmes();
+  return res.status(200).json(filmes);
 };
 
 export const adicionarFilme = (req: Request, res: Response): Response => {
-    const novoFilme = filmesService.adicionarFilme(req.body);
-    return res.status(201).json(novoFilme);
+  const novoFilme = filmesService.adicionarFilme(req.body);
+  return res.status(201).json(novoFilme);
 };
 
 export const deletarFilme = (req: Request, res: Response): Response => {
-    try {
-        const id = parseInt(req.params.id);
-        filmesService.deletarFilme(id); 
-        return res.status(204).send();
-    } catch (error) {
-        if (error instanceof Error) {
-            return res.status(404).json({ message: error.message });
-        }
-        return res.status(404).json({ message: "Filme não encontrado" });
-    }
+  try {
+    const id = parseInt(req.params.id);
+    filmesService.deletarFilme(id);
+    return res.status(204).send();
+  } catch (error) {
+    return res.status(404).json({
+      message: error instanceof Error ? error.message : "Erro desconhecido",
+    });
+  }
 };
